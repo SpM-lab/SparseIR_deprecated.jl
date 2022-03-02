70565b53103cd2b2062acadf3e4ad2de299362a6
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2022-03-02 15:01:11 +0900

    Change conda channel to spm-lab

--- src/SparseIR.jl
+++ src/SparseIR.jl
@@ -6,7 +6,7 @@ const sparse_ir = PyNULL()
 const pyspr = PyNULL()
 
 function __init__()
-    copy!(sparse_ir, pyimport_conda("sparse_ir", "sparse-ir", "h.shinaoka"))
+    copy!(sparse_ir, pyimport_conda("sparse_ir", "sparse-ir", "spm-lab"))
     copy!(pyspr, pyimport("sparse_ir.spr"))
     check_version_sparse_ir()
 end
