49d9247f0260098113e6d7cd028a744d26ab6780
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2022-03-19 21:13:37 +0900

    Reimplement DecomposedMatrix in Julia

--- src/sampling.jl
+++ src/sampling.jl
@@ -1,23 +1,121 @@
+import LinearAlgebra: svd
+
 ######### DecomposedMatrix ########
-struct DecomposedMatrix
-    o::PyObject
+
+"""
+Matrix in SVD decomposed form for fast and accurate fitting.
+
+Stores a matrix ``A`` together with its thin SVD form::
+
+    A == (u * s) * vH.
+
+This allows for fast and accurate least squares fits using ``lstsq(A, x)``.
+"""
+struct DecomposedMatrix{T}
+    a::Matrix{T}
+    uH::Matrix{T}
+    s::Vector{Float64}
+    v::Matrix{T}
 end
 
-function DecomposedMatrix(a::Matrix{T}) where T
-    DecomposedMatrix(pysampling.DecomposedMatrix(a))
+function DecomposedMatrix{T}(
+        a::Matrix{T},
+        svd_result::Union{Tuple{Matrix{T},Vector{Float64},Matrix{T}},Nothing} = nothing
+    ) where T
+    if svd_result === nothing
+        F = svd(a)
+        uH, s, v = collect(F.U'), F.S, collect(F.V)
+    else
+        u, s, vH = svd_result
+        uH = collect(u')
+        v = collect(vH')
+    end
+    DecomposedMatrix(a, uH, s, v)
+end
+
+function _move_axis(arr::Array{T,N}, src::Int, dst::Int)::Array{T,N} where {T, N}
+    """
+    Move the axis at src to dst with keeping the order of the rest axes unchanged.
+
+    src=1, dst=2, N=4, perm=[2, 1, 3, 4]
+    src=2, dst=4, N=4, perm=[1, 3, 4, 2]
+    """
+    if src == dst
+        return arr
+    end
+    perm = collect(1:N)
+    deleteat!(perm, src)
+    insert!(perm, dst, src)
+    permutedims(arr, perm)
+end
+
+"""
+Apply a matrix operator to an array along a given axis
+"""
+function _matop_along_axis(op::Matrix{T}, arr::Array{S,N}, axis::Int64) where {T, S, N}
+    # Move the target axis to the first position
+    if axis < 0 || axis > N
+        throw(DomainError("axis must be in [1,N]"))
+    end
+    if size(arr)[axis] != size(op)[2]
+        error("Dimension mismatch!")
+    end
+
+    arr = _move_axis(arr, axis, 1)
+    _move_axis(_matop(op, arr), 1, axis)
+end
+
+function _matop(op::Matrix{T}, arr::Array{S,N}) where {T, S, N}
+    # Apply op to the first axis of an array
+    if size(arr)[1] != size(op)[2]
+        error("Dimension mismatch!")
+    end
+    if N == 1
+        return op * arr
+    end
+
+    rest_dims = size(arr)[2:end]
+    arr = reshape(arr, (size(op)[2], prod(rest_dims)))
+    reshape(op * arr, (size(op)[1], rest_dims...))
 end
 
 function matmul(matrix::DecomposedMatrix, al::Array{T,N}, axis::Int64)::Array{ComplexF64,N} where {T, N}
-    matrix.o.matmul(al, axis-1)
+    _matop_along_axis(matrix.a, al, axis)
 end
 
 function lstsq(matrix::DecomposedMatrix, ax::Array{T,N}, axis::Int64)::Array{ComplexF64,N} where {T, N}
-    matrix.o.lstsq(ax, axis-1)
+    ax = _move_axis(ax, axis, 1)
+    r = _matop(matrix.uH, ax)
+    for j = 1:size(r, 2)
+        for i = 1:size(r, 1)
+            r[i,j] /= matrix.s[i]
+        end
+    end
+    res = _matop(matrix.v, r)
+    _move_axis(res, 1, axis)
 end
 
-cond(a::DecomposedMatrix) = a.o.s[1]/a.o.s[length(a.o.s)]
+cond(a::DecomposedMatrix) = a.s[1]/a.s[length(a.s)]
 
 ############# Sampling ############
+"""Base class for sparse sampling.
+
+Encodes the "basis transformation" of a propagator from the truncated IR
+basis coefficients ``G_ir[l]`` to time/frequency sampled on sparse points
+``G(x[i])`` together with its inverse, a least squares fit::
+
+         ________________                   ___________________
+        |                |    evaluate     |                   |
+        |     Basis      |---------------->|     Value on      |
+        |  coefficients  |<----------------|  sampling points  |
+        |________________|      fit        |___________________|
+
+
+Attributes:
+    basis: IR Basis instance
+    matrix: Evaluation matrix is decomposed form
+    sampling_points: Set of sampling points
+"""
 abstract type SamplingBase end
 
 """
@@ -51,7 +149,7 @@ in (scaled/unscaled) imaginary time.
 struct TauSampling <: SamplingBase
     basis::Basis
     sampling_points::Vector{Float64}
-    matrix::DecomposedMatrix
+    matrix::DecomposedMatrix{Float64}
     cond::Float64
 end
 
@@ -60,7 +158,7 @@ function TauSampling(basis::Basis, sampling_points::Union{Nothing,Vector{Float64
     sampling_points = sampling_points === nothing ?
          default_tau_sampling_points(basis) : sampling_points
     mat_ = Matrix{Float64}(transpose(basis.u(sampling_points)))
-    matrix = DecomposedMatrix(mat_)
+    matrix = DecomposedMatrix{Float64}(mat_)
     TauSampling(basis, sampling_points, matrix, cond(matrix))
 end
 
@@ -80,13 +178,13 @@ Sampling in Matsubara frequency
 struct MatsubaraSampling <: SamplingBase
     basis::Basis
     sampling_points::Vector{Int64}
-    matrix::DecomposedMatrix
+    matrix::DecomposedMatrix{ComplexF64}
     cond::Float64
 end
 
 function MatsubaraSampling(basis::Basis, sampling_points::Union{Nothing,Vector{Int64}}=nothing)
     sampling_points = sampling_points === nothing ?
          default_matsubara_sampling_points(basis) : sampling_points
-    matrix = DecomposedMatrix(Matrix{ComplexF64}(transpose(basis.uhat(sampling_points))))
+    matrix = DecomposedMatrix{ComplexF64}(Matrix{ComplexF64}(transpose(basis.uhat(sampling_points))))
     MatsubaraSampling(basis, sampling_points, matrix, cond(matrix))
 end
--- test/test_sampling.jl
+++ test/test_sampling.jl
@@ -1,6 +1,36 @@
 using SparseIR
 using Test
 
+@testset "sampling.moveaxis" begin
+    shape = (2, 3, 4)
+    x = reshape(collect(1:prod(shape)), shape)
+    ref = zeros(Int64, 3, 4, 2)
+    for k in 1:4
+        for j in 1:3
+            for i in 1:2
+                ref[j, k, i] = x[i, j, k]
+            end
+        end
+    end
+    res = SparseIR._move_axis(x, 1, 3)
+    @test all(res .== ref)
+end
+
+@testset "sampling._matop_along_axis" begin
+    op = reshape(collect(1:6), (3,2))
+    x = reshape(collect(1:4), (2,2))
+    res = SparseIR._matop_along_axis(op, x, 2)
+    ref = zeros(Int64, (2,3))
+    for j in 1:2
+        for i in 1:3
+            for k in 1:2
+                ref[j, i] += op[i, k] * x[j, k]
+            end
+        end
+    end
+    @test all(res .== ref)
+end
+
 @testset "sampling.XSampling" begin
     beta = 1e+4
     wmax = 1.
