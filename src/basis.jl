d245665288f6c34b5840a10fb2e6a4be181e032d
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2022-03-24 16:51:50 +0900

    Cosmetic changes

--- src/basis.jl
+++ src/basis.jl
@@ -96,9 +96,7 @@ function FiniteTempBasis(statistics::Statistics, beta::Real, wmax::Real,
                          kernel::Union{KernelBase,Nothing}=nothing)
     kernel_py = kernel !== nothing ? kernel.o : nothing
     stat = statistics == fermion ? "F" : "B"
-    if beta < 2e-8
-        error("xprec is not supported yet.")
-    end
+    beta < 2e-8 || error("xprec is not supported yet.")
     o = sparse_ir.FiniteTempBasis(stat, Float64(beta), Float64(wmax); eps=eps,
                                   kernel=kernel_py)
     return FiniteTempBasis(o)
